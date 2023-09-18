803d3dd05c5121758172a637327223732dd67ec6
I-Joyeux <143741304+I-Joyeux@users.noreply.github.com>
2023-09-18 10:02:44 -0600

    Ch2 Programming   (planets anum)

---
+++ Planets/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+class DisplayPlanet
+{
+    enum Planets
+    {
+        Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune,
+
+    }
+
+    static void Main()
+    {
+     
+        int PlanetsNum;
+        Planets planetsName;
+        WriteLine("Enter a planet number");
+        PlanetsNum=Convert.ToInt32(Console.ReadLine());
+
+        planetsName = (Planets)PlanetsNum-1;
+        Console.WriteLine("{0} is Planets {1}",planetsName.ToString(),PlanetsNum.ToString());
+    
+    }
+
+}
